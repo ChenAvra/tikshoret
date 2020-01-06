3e59a2d8107f414893cd7747a35f191686a52601
Chen <chenavra@post.bgu.ac.il>
2020-01-06 17:39:16 +0200

    b

--- src/client.java
+++ src/client.java
@@ -1,6 +1,9 @@
+import sun.security.x509.IPAddressName;
+
 import java.io.*;
 import java.net.*;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.concurrent.TimeoutException;
 
 public class client {
@@ -8,12 +11,12 @@ public class client {
     public String hash;
     public int length;
     public String teamName;
-    public HashMap<InetAddress, String >listServer;
+    public HashSet<InetAddress> listServer;
     public DatagramSocket data;
 
     public client(String teamName) {
         this.teamName=teamName;
-        this.listServer=new HashMap<>();
+        this.listServer=new HashSet<>();
 
     }
 
@@ -40,89 +43,138 @@ public class client {
                 byte[] receiveData = new byte[1024];
                 DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
                 clientSocket.receive(receivePacket);
+                message mFromServer=new message(new String (receivePacket.getData()));
+                if(mFromServer.getType()=='2'){
+                    listServer.add(receivePacket.getAddress());
+                }
             } catch (SocketTimeoutException te) {
                 isTimeOut = true;
+                System.out.println("1 sec passed");
 
 
             }
-            clientSocket.close();
-        }
-    }
 
-    public void start() throws IOException {
-        System.out.println("welcome to "+teamName+". Please enter the hash");
-        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-        String sentence = inFromUser.readLine();
-        this.hash=sentence;
-        System.out.println("Please enter the input string length");
-        BufferedReader inFromUser2= new BufferedReader(new InputStreamReader(System.in));
-        int   length  = Integer.parseInt(inFromUser2.readLine());
-        this.length=length;
-        discoverAndOffer(sentence,length);
+        }
+        sendRequest(clientSocket);
 
     }
 
+    public  void  sendRequest( DatagramSocket clientSocket) throws IOException {
+        String[] ranges = divideToDomains(length, listServer.size());
+        int i = 0;
+        for (InetAddress ip1 : listServer) {
+            if (i < ranges.length - 1) {
+                message m = new message(teamName.toCharArray(), '3', hash.toCharArray(), (char) length, ranges[i].toCharArray(), ranges[i + 1].toCharArray());
+                i++;
+                byte[] sendData = new byte[1024];
+                sendData = m.getFullString().getBytes();
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ip1, 3117);
+                clientSocket.send(sendPacket);
 
 
-    private String converxtIntToString(int toConvert, int length) {
-        StringBuilder s = new StringBuilder(length);
-        while (toConvert > 0 ){
-            int c = toConvert % 26;
-            s.insert(0, (char) (c + 'a'));
-            toConvert /= 26;
-            length --;
-        }
-        while (length > 0){
-            s.insert(0, 'a');
-            length--;
-        }
-        return s.toString();
-    }
-
-    private String [] divideToDomains (int stringLength, int numOfServers){
-        String [] domains = new String[numOfServers * 2];
-
-        StringBuilder first = new StringBuilder(); //aaa
-        StringBuilder last = new StringBuilder(); //zzz
-
-        for(int i = 0; i < stringLength; i++){
-            first.append("a"); //aaa
-            last.append("z"); //zzz
-        }
-
-        int total = convertStringToInt(last.toString());
-        int perServer = (int) Math.floor (((double)total) /  ((double)numOfServers));
-
-        domains[0] = first.toString(); //aaa
-        domains[domains.length -1 ] = last.toString(); //zzz
-        int summer = 0;
-
-        for(int i = 1; i <= domains.length -2; i += 2){
-            summer += perServer;
-            domains[i] = converxtIntToString(summer, stringLength); //end domain of server
-            summer++;
-            domains[i + 1] = converxtIntToString(summer, stringLength); //start domain of next server
-        }
-
-        return domains;
-    }
+                clientSocket.setSoTimeout(1000*15000);
+                boolean isTimeOut = false;
+                boolean found=false;
+                while (!isTimeOut && !found) {
+                    try {
+                        byte[] receiveData = new byte[1024];
+                        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                        clientSocket.receive(receivePacket);
+                        message mFromServer = new message(new String(receivePacket.getData()));
+                        if (mFromServer.getType() == '4') {
+                                String answer=mFromServer.getOriginalStringStart().toString();
+                                System.out.println(answer);
+                                found=true;
+                        }
+                    } catch (SocketTimeoutException te) {
+                        isTimeOut = true;
+                        System.out.println("15 sec passed");
 
 
+                    }
 
-    private int convertStringToInt(String toConvert) {
-        char[] charArray = toConvert.toCharArray();
-        int num = 0;
-        for(char c : charArray){
-            if(c < 'a' || c > 'z'){
-                throw new RuntimeException();
+
+                }
+
+                clientSocket.close();
+
+                System.out.println("there is no string for hash");
             }
-            num *= 26;
-            num += c - 'a';
         }
-        return num;
     }
 
-}
+        public void start () throws IOException {
+            System.out.println("welcome to " + teamName + ". Please enter the hash");
+            BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+            String sentence = inFromUser.readLine();
+            this.hash = sentence;
+            System.out.println("Please enter the input string length");
+            BufferedReader inFromUser2 = new BufferedReader(new InputStreamReader(System.in));
+            int length = Integer.parseInt(inFromUser2.readLine());
+            this.length = length;
+            discoverAndOffer(sentence, length);
+
+        }
+
+
+        private String converxtIntToString ( int toConvert, int length){
+            StringBuilder s = new StringBuilder(length);
+            while (toConvert > 0) {
+                int c = toConvert % 26;
+                s.insert(0, (char) (c + 'a'));
+                toConvert /= 26;
+                length--;
+            }
+            while (length > 0) {
+                s.insert(0, 'a');
+                length--;
+            }
+            return s.toString();
+        }
+
+        private String[] divideToDomains ( int stringLength, int numOfServers){
+            String[] domains = new String[numOfServers * 2];
+
+            StringBuilder first = new StringBuilder(); //aaa
+            StringBuilder last = new StringBuilder(); //zzz
+
+            for (int i = 0; i < stringLength; i++) {
+                first.append("a"); //aaa
+                last.append("z"); //zzz
+            }
+
+            int total = convertStringToInt(last.toString());
+            int perServer = (int) Math.floor(((double) total) / ((double) numOfServers));
+
+            domains[0] = first.toString(); //aaa
+            domains[domains.length - 1] = last.toString(); //zzz
+            int summer = 0;
+
+            for (int i = 1; i <= domains.length - 2; i += 2) {
+                summer += perServer;
+                domains[i] = converxtIntToString(summer, stringLength); //end domain of server
+                summer++;
+                domains[i + 1] = converxtIntToString(summer, stringLength); //start domain of next server
+            }
+
+            return domains;
+        }
+
+
+        private int convertStringToInt (String toConvert){
+            char[] charArray = toConvert.toCharArray();
+            int num = 0;
+            for (char c : charArray) {
+                if (c < 'a' || c > 'z') {
+                    throw new RuntimeException();
+                }
+                num *= 26;
+                num += c - 'a';
+            }
+            return num;
+        }
+    }
+
 
 
 
